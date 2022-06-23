8aefebefd3cc6ea8e95ca4d527ca9161ccdd8dc8
SalvatorePortera <106048648+SalvatorePortera@users.noreply.github.com>
2022-06-23 01:33:42 -0400

    tsetett

--- resources/views/chequer/payee/ledger.blade.php
+++ resources/views/chequer/payee/ledger.blade.php
@@ -223,6 +223,9 @@
                                     <span><label>Bill Number:</label>{{$data->invoice_no}}</span>
                                 </td>
                                 <td>
+                                    @php
+                                        print_r($data);
+                                    @endphp
                                     {{$data->payment_type}}
                                 </td>
                                 <td>{{$data->location_name}}</td>
