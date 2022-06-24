14b3e7da1fa6046f496914b1970f02528206ef17
SalvatorePortera <106048648+SalvatorePortera@users.noreply.github.com>
2022-06-24 18:25:12 +0530

    tsetst

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -45,7 +45,7 @@ use Modules\Fleet\Entities\Fleet;
 use Modules\Petro\Entities\SettlementExpensePayment;
 
 use Yajra\DataTables\Facades\DataTables;
-
+use App\Providers\AppServiceProvider;
 
 
 class ExpenseController extends Controller
@@ -418,7 +418,7 @@ class ExpenseController extends Controller
 
                     
                     // $packages->default_setting
-                    return '<a href="'.action("TransactionPaymentController@show", [$row->id]).'" class="view_payment_modal payment-status no-print" data-orig-value="'.$row->payment_status.'" data-status-name="'.__('lang_v1.'.$row->payment_status).'"><span class="label '.payment_status($row->payment_status).'">'.__('lang_v1.' . $row->payment_status).'</span></a><span class="print_section">'.__('lang_v1.' . $row->payment_status).'</span>';
+                    return '<a href="'.action("TransactionPaymentController@show", [$row->id]).'" class="view_payment_modal payment-status no-print" data-orig-value="'.$row->payment_status.'" data-status-name="'.__('lang_v1.'.$row->payment_status).'"><span class="label '.AppServiceProvider::payment_status($row->payment_status).'">'.__('lang_v1.' . $row->payment_status).'</span></a><span class="print_section">'.__('lang_v1.' . $row->payment_status).'</span>';
 
                 })
                 
