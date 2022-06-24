0714b137e7d8533048116e7f778f4c1fb5c3f018
SalvatorePortera <106048648+SalvatorePortera@users.noreply.github.com>
2022-06-24 19:56:41 +0530

    dddddddddddddddddd

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -436,7 +436,7 @@ class ExpenseController extends Controller
                 ->editColumn('payment_status', function ($row) {
 
                     $package_manage = Package::where('only_for_business', $row->business_id)->first();
-                    print_r($package_manage);
+                    
                     if($package_manage->auto_update_payment_status)
                         return '<a href="'.action("TransactionPaymentController@show", [$row->id]).'" class="view_payment_modal payment-status no-print" data-orig-value="'.$row->payment_status.'" data-status-name="'.__('lang_v1.'.$row->payment_status).'"><span class="label '.$this->__payment_status($row->payment_status).'">'.__('lang_v1.' . $row->payment_status).'</span></a><span class="print_section">'.__('lang_v1.' . $row->payment_status).'</span>';
                     else
