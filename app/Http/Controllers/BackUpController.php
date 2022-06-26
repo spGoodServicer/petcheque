f916177cc8ee066f452926ac57f7e124d360e6b3
SalvatorePortera <106048648+SalvatorePortera@users.noreply.github.com>
2022-06-26 16:07:21 +0900

    fdasfasdf

--- app/Http/Controllers/BackUpController.php
+++ app/Http/Controllers/BackUpController.php
@@ -101,29 +101,14 @@ class BackUpController extends Controller
             abort(403, 'Unauthorized action.');
         }
 
-        //Disable in demo
-        if (config('app.env') == 'demo') {
-            $output = ['success' => 0,
-                            'msg' => 'Feature disabled in demo!!'
-                        ];
-            return back()->with('status', $output);
-        }
+        $path = config('backupmanager.backups.backup_path') . DIRECTORY_SEPARATOR . $file;
 
-        $file = config('backup.backup.name') . '/' . $file_name;
-        $disk = Storage::disk(config('backup.backup.destination.disks')[0]);
-        if ($disk->exists($file)) {
-            $fs = Storage::disk(config('backup.backup.destination.disks')[0])->getDriver();
-            $stream = $fs->readStream($file);
-            return \Response::stream(function () use ($stream) {
-                fpassthru($stream);
-            }, 200, [
-                "Content-Type" => $fs->getMimetype($file),
-                "Content-Length" => $fs->getSize($file),
-                "Content-disposition" => "attachment; filename=\"" . basename($file) . "\"",
-            ]);
-        } else {
-            abort(404, "The backup file doesn't exist.");
-        }
+        $file = Storage::disk(config('backupmanager.backups.disk'))
+                ->getDriver()
+                ->getAdapter()
+                ->getPathPrefix() . $path;
+
+        return response()->download($file);
     }
 
     /**
