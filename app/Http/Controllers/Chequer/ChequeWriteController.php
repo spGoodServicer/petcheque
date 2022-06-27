88dfba81cd6e3927030a5d3590642d262e865779
SalvatorePortera <salvap0rtera1@gmail.com>
2022-06-27 10:02:11 +0800

    dddddddddddddddd

--- app/Http/Controllers/Chequer/ChequeWriteController.php
+++ app/Http/Controllers/Chequer/ChequeWriteController.php
@@ -266,15 +266,13 @@ class ChequeWriteController extends Controller
                                     ];
                                     AccountTransaction::createAccountTransaction($ob_transaction_data);
                                 }
-                            }       
+                            }
+                            //update payment status
+                            $this->transactionUtil->updatePaymentStatus($transaction_id, $transaction->final_total);
+                            $inputs['transaction_type'] = $transaction->type;
+                            event(new TransactionPaymentAdded($tp, $inputs));       
                         }
-                        //update payment status
-                        $this->transactionUtil->updatePaymentStatus($transaction_id, $transaction->final_total);
-                        $inputs['transaction_type'] = $transaction->type;
-                        event(new TransactionPaymentAdded($tp, $inputs));
                     }
-                    
-                    
                     DB::commit();
                 }
 
